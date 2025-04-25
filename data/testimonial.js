ce3d48395591dfe09a8836a426b3bff0683ceb49
fatema-a-ferdaus <fatema.a.ferdaus@gmail.com>
2025-04-25 23:25:24 +0600

    finished

--- data/testimonial.js
+++ data/testimonial.js
@@ -5,20 +5,7 @@ export const testimonialList = [
     comment:
       "Lorem ipsum dolor sit amet, consectetur ad-zzz-ing elit. Proin vel urna at metus tempor mattis, or not, whatever.",
     avater: "assets/images/avater/Avatar.png",
-  },
-  {
-    name: "Oarack Babama",
-    designation: "Former President of US",
-    comment:
-      "Nisi porta lorem mollis aliquam ut porttitor leo. Sed odio morbi quis commodo. Arcu non odio euismod lacinia at quis. Nec sagittis aliquam malesuada bibendum arcu vitae elementum curabitur vitae.",
-    avater: "assets/images/avater/Avatar(1).png",
-  },
-  {
-    name: "Boe Jiden",
-    designation: "Former President Of Canada",
-    comment:
-      "Sagittis eu volutpat odio facilisis mauris sit amet massa. Urna et pharetra pharetra massa.",
-    avater: "assets/images/avater/Avatar(2).png",
+    rating: 5,
   },
   {
     name: "Azunyan U. Wu",
@@ -26,20 +13,7 @@ export const testimonialList = [
     comment:
       "Posuere urna nec tincidunt praesent semper feugiat nibh sed. Velit dignissim sodales ut eu.",
     avater: "assets/images/avater/Avatar(3).png",
-  },
-  {
-    name: "Saylor Twift",
-    designation: "Famous Singer",
-    comment:
-      "Accumsan tortor posuere ac ut consequat. Quis varius quam quisque id diam vel. Commodo odio aenean sed adipiscing diam donec adipiscing. Vel pharetra vel turpis nunc eget.",
-    avater: "assets/images/avater/Avatar(4).png",
-  },
-  {
-    name: "Vermillion D. Gray",
-    designation: "CEO, hacklife.ai",
-    comment:
-      " Cras fermentum odio eu feugiat pretium nibh ipsum consequat nisl. Nec nam aliquam sem et tortor consequat id porta nibh. Viverra suspendisse. Lacus sed viverra tellus in hac habitasse platea dictumst. Elit at imperdiet dui accumsan.",
-    avater: "assets/images/avater/Avatar(5).png",
+    rating: 3.5,
   },
   {
     name: "Mechatronics Yi",
@@ -47,6 +21,23 @@ export const testimonialList = [
     comment:
       "Sit amet est placerat in. Blandit cursus risus at ultrices mi tempus imperdiet. Volutpat commodo sed egestas egestas fringilla phasellus. Tincidunt eget nullam non nisi. ",
     avater: "assets/images/avater/Avatar(6).png",
+    rating: 4,
+  },
+  {
+    name: "Oarack Babama",
+    designation: "Former President of US",
+    comment:
+      "Nisi porta lorem mollis aliquam ut porttitor leo. Sed odio morbi quis commodo. Arcu non odio euismod lacinia at quis. Nec sagittis aliquam malesuada bibendum arcu vitae elementum curabitur vitae.",
+    avater: "assets/images/avater/Avatar(1).png",
+    rating: 5,
+  },
+  {
+    name: "Saylor Twift",
+    designation: "Famous Singer",
+    comment:
+      "Accumsan tortor posuere ac ut consequat. Quis varius quam quisque id diam vel. Commodo odio aenean sed adipiscing diam donec adipiscing. Vel pharetra vel turpis nunc eget.",
+    avater: "assets/images/avater/Avatar(4).png",
+    rating: 4.5,
   },
   {
     name: "Asuna Yuuki",
@@ -54,6 +45,23 @@ export const testimonialList = [
     comment:
       "Venenatis cras sed felis eget velit aliquet. Velit scelerisque in dictum non consectetur. Felis eget velit aliquet sagittis. Ultricies integer quis auctor elit sed vulputate mi. ",
     avater: "assets/images/avater/Avatar(7).png",
+    rating: 3.5,
+  },
+  {
+    name: "Boe Jiden",
+    designation: "Former President Of Canada",
+    comment:
+      "Sagittis eu volutpat odio facilisis mauris sit amet massa. Urna et pharetra pharetra massa.",
+    avater: "assets/images/avater/Avatar(2).png",
+    rating: 2.5,
+  },
+  {
+    name: "Vermillion D. Gray",
+    designation: "CEO, hacklife.ai",
+    comment:
+      " Cras fermentum odio eu feugiat pretium nibh ipsum consequat nisl. Nec nam aliquam sem et tortor consequat id porta nibh. Viverra suspendisse. Lacus sed viverra tellus in hac habitasse platea dictumst. Elit at imperdiet dui accumsan.",
+    avater: "assets/images/avater/Avatar(5).png",
+    rating: 5,
   },
   {
     name: "Zuckman Wu",
@@ -61,5 +69,6 @@ export const testimonialList = [
     comment:
       "Viverra accumsan in nisl nisi scelerisque eu. Vitae congue eu consequat ac felis donec.",
     avater: "assets/images/avater/Avatar(8).png",
+    rating: 4,
   },
 ];
--- script/script.js
+++ script/script.js
@@ -1,10 +1,10 @@
 import { companyPhotos } from "../data/companyPhoto.js";
 import { faqList } from "../data/FAQs.js";
 import { primaryFeatures } from "../data/features.js";
+import { footerData } from "../data/footerData.js";
 import { officeLocations } from "../data/officeLocation.js";
+import { emptyStar, fullStar, halfStar } from "../data/star.js";
 import { testimonialList } from "../data/testimonial.js";
-
-console.log(faqList);
 
 // social proof
 const socialProof = document.getElementById("social-proof");
@@ -43,45 +43,89 @@ primaryFeatures.forEach((feature) => {
 });
 
 // testimonilas
-const colCount = 3;
-const columns = Array.from({ length: colCount }, () => []);
-
-testimonialList.forEach((testimonial, index) => {
-  const colIndex = index % colCount;
-  columns[colIndex].push(testimonial);
-});
 const testimonialContainer = document.getElementById("testimonial-list");
+const loadMoreBtn = document.getElementById("more-testimonial-btn");
 
-columns.forEach((colData) => {
-  const col = document.createElement("div");
-  col.className = "testimonial-card-container ";
-  colData.forEach(({ name, designation, comment, avater }) => {
-    const card = document.createElement("div");
-    card.className = "testimonial-card";
-    card.innerHTML = `
-      <div class="testimonial-rating">********</div>
+const colCount = 3; // 3 columns layout on desktop
+const isMobile = window.innerWidth < 768;
+let visibleCount = isMobile ? 4 : testimonialList.length; //mobile show 4 cards intialy
 
-                <div class="paragraph-lg">
-                 ${comment}
-                </div>
+function renderTestimonials(limit) {
+  testimonialContainer.innerHTML = "";
+  const columns = Array.from({ length: colCount }, () => []);
 
-                <div class="testimonial-profile">
-                  <span class="profile-img">
-                    <img
-                      src="${avater}"
-                    />
-                  </span>
-
-                  <span class="testimonial-profile-detail">
-                    <span class="profile-name-text"> ${name}</span>
-                    <span class="commenter-info-text">${designation}</span>
-                  </span>
-                </div>
-    `;
-    col.appendChild(card);
+  testimonialList.slice(0, limit).forEach((testimonial, index) => {
+    const colIndex = index % colCount;
+    columns[colIndex].push(testimonial);
   });
-  testimonialContainer.appendChild(col);
+
+  columns.forEach((colData) => {
+    const col = document.createElement("div");
+    col.className = "testimonial-card-container";
+
+    colData.forEach(({ name, designation, comment, avater, rating }) => {
+      const card = document.createElement("div");
+      card.className = "testimonial-card";
+      const ratingHTML = renderRating(rating); //star render
+
+      card.innerHTML = `
+        <div class="testimonial-rating">${ratingHTML}</div>
+        <div class="paragraph-lg">${comment}</div>
+
+        <div class="testimonial-profile">
+          <span class="profile-img">
+            <img src="${avater}" />
+          </span>
+          <span class="testimonial-profile-detail">
+            <span class="profile-name-text">${name}</span>
+            <span class="commenter-info-text">${designation}</span>
+          </span>
+        </div>
+      `;
+      col.appendChild(card);
+    });
+
+    testimonialContainer.appendChild(col);
+  });
+
+  // hide button if no card
+  if (limit >= testimonialList.length) {
+    loadMoreBtn.style.display = "none";
+  }
+}
+renderTestimonials(visibleCount); // 1st time render
+
+// load more click
+loadMoreBtn.addEventListener("click", () => {
+  visibleCount += 3;
+  renderTestimonials(visibleCount);
 });
+
+// rating star
+function renderRating(rating) {
+  let starsHTML = "";
+  const full = Math.floor(rating);
+  const hasHalf = rating % 1 >= 0.5;
+  const total = 5;
+
+  // full stars
+  for (let i = 0; i < full; i++) {
+    starsHTML += fullStar;
+  }
+
+  //half star (only one)
+  if (hasHalf) {
+    starsHTML += halfStar;
+  }
+
+  // empty stars
+  const empty = total - full - (hasHalf ? 1 : 0);
+  for (let i = 0; i < empty; i++) {
+    starsHTML += emptyStar;
+  }
+
+  return starsHTML;
+}
 
 // office location
 const officeList = document.getElementById("contact-list");
@@ -138,5 +182,83 @@ faqList.forEach((faq) => {
     answerPara.classList.toggle("show");
     alignStart.classList.toggle("align-start");
     toggleBtn.classList.toggle("rotate");
+  });
+});
+
+// footer links
+const renderLinks = (list, containerId) => {
+  const container = document.getElementById(containerId);
+  list.forEach((item) => {
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    a.href = item.link;
+    a.textContent = item.title;
+    li.appendChild(a);
+    container.appendChild(li);
+  });
+};
+
+// Render the lists dynamically
+document.addEventListener("DOMContentLoaded", () => {
+  renderLinks(footerData.siteMap, "siteMap-list"); // Site Map
+  renderLinks(footerData.services, "services-list"); // Services
+  renderLinks(footerData.products, "products-list"); // Product
+  renderLinks(footerData.helpAndSupport, "help-support-list"); // Help & Support
+  renderLinks(footerData.legal, "legal-list"); // Legal
+  renderLinks(footerData.pricing, "pricing-list"); // Pricing
+});
+
+// interactive behaviours
+// scroll to top
+document.getElementById("bottom-btn").addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth", // Smooth scrolling effect
+  });
+});
+
+// cards into view
+const observer = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach((entry) => {
+      const card = entry.target;
+      const bar = card.querySelector(".bar");
+
+      document.querySelectorAll(".who-is-it-for-card .bar").forEach((bar) => {
+        bar.style.backgroundColor = "#e0e7ff";
+      });
+
+      if (entry.isIntersecting) {
+        bar.style.backgroundColor = "#4f46e5"; // change the bar color
+      }
+    });
+  },
+  {
+    threshold: 0.55,
+  }
+);
+//  each card
+document.querySelectorAll(".who-is-it-for-card").forEach((card) => {
+  observer.observe(card);
+});
+
+// mobile nav modal
+const menuBtn = document.getElementById("mobile-menu-btn");
+const modal = document.querySelector(".mobile-menu-modal");
+const closeBtn = document.getElementById("modal-close-btn");
+
+menuBtn.addEventListener("click", () => {
+  modal.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.classList.remove("active");
+});
+
+const navLinks = document.querySelectorAll(".mobile-nav-menu a");
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    modal.classList.remove("active");
   });
 });
